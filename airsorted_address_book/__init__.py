13d3923cff1b92e3577731a071bc7ead0ceb2d62
Piotr Matuszkiewicz <piotr.matuszkiewicz@gmail.com>
2017-10-30 02:39:51 +0100

    Adds SQLALCHEMY_TRACK_MODIFICATIONS config option to get rid of warning

--- airsorted_address_book/__init__.py
+++ airsorted_address_book/__init__.py
@@ -10,6 +10,7 @@ def create_app(config_name=None):
         app.config.from_envvar('ADDRESS_BOOK_CONFIG', silent=True)
     else:
         app.config.from_pyfile('config/'+config_name)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     db.app = app
     db.create_all()
