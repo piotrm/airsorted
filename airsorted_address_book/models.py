1bd27494d88c0e5bab99debb1784cc278128467b
Piotr Matuszkiewicz <piotr.matuszkiewicz@gmail.com>
2017-10-30 22:09:18 +0100

    Fixes Contact-Email relations; adds Marshmallow schemas

--- airsorted_address_book/models.py
+++ airsorted_address_book/models.py
@@ -1,4 +1,8 @@
-from airsorted_address_book import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow, fields
+
+db = SQLAlchemy()
+ma = Marshmallow()
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -6,11 +10,34 @@ class Contact(db.Model):
     last_name = db.Column(db.String(80), nullable = False)
     company = db.Column(db.String(140))
     emails = db.relationship('Email',
-        backref=db.backref('contact', lazy='joined'))
+        backref='contact', lazy='dynamic', cascade='save-update,delete')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 
 class Email(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     address = db.Column(db.String(80), nullable = False)
     contact_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
-    contact = db.relationshiop('Contact',
-        backref=db.backref('emails', lazy='joined')) 
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+
+class EmailSchema(ma.ModelSchema):
+    class Meta:
+        model = Email
+
+class ContactSchema(ma.ModelSchema):
+    class Meta:
+        model = Contact
+    emails = ma.Nested(EmailSchema, many=True)
